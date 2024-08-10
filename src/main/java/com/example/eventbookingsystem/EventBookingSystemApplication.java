9c0adeceb924baa85a342623bde4fe0f842a278d
shengzan <127278566+shengzan@users.noreply.github.com>
2024-08-10 19:51:53 -0400

    fix: load environment variables for MySQL connection

--- src/main/java/com/example/eventbookingsystem/EventBookingSystemApplication.java
+++ src/main/java/com/example/eventbookingsystem/EventBookingSystemApplication.java
@@ -2,11 +2,15 @@ package com.example.eventbookingsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class EventBookingSystemApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("MYSQL_USERNAME", dotenv.get("MYSQL_USERNAME"));
+        System.setProperty("MYSQL_PASSWORD", dotenv.get("MYSQL_PASSWORD"));
         SpringApplication.run(EventBookingSystemApplication.class, args);
     }
 }
