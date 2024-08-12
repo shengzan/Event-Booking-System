9531696db775d969bfb96b02696662725171218e
shengzan <127278566+shengzan@users.noreply.github.com>
2024-08-11 23:40:57 -0400

    fix: Inject JwtTokenProvider dependency using Autowired

--- src/main/java/com/example/eventbookingsystem/security/JwtTokenProvider.java
+++ src/main/java/com/example/eventbookingsystem/security/JwtTokenProvider.java
@@ -2,6 +2,8 @@ package com.example.eventbookingsystem.security;
 
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
