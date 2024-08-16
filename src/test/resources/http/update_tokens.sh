0a3dee9221e00244ee21159ad5fa217a857e71ef
shengzan <127278566+shengzan@users.noreply.github.com>
2024-08-15 20:59:43 -0400

    feat: Add script to update tokens in http-client.env.json

---
+++ src/test/resources/http/update_tokens.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Set the host
+HOST="http://localhost:8080"
+
+# Function to extract token from response
+extract_token() {
+    echo "$1" | grep -o '"token":"[^"]*' | sed 's/"token":"//'
+}
+
+# Login as organizer
+organizer_response=$(curl -s -X POST "$HOST/api/users/login" \
+     -H "Content-Type: application/json" \
+     -d '{"username": "organizer", "password": "123456"}')
+
+organizer_token=$(extract_token "$organizer_response")
+
+# Login as user
+user_response=$(curl -s -X POST "$HOST/api/users/login" \
+     -H "Content-Type: application/json" \
+     -d '{"username": "jason", "password": "123456"}')
+
+user_token=$(extract_token "$user_response")
+
+# Update http-client.env.json
+jq --arg organizer_token "$organizer_token" --arg user_token "$user_token" \
+   '.dev.authToken = $organizer_token | .dev.userAuthToken = $user_token' \
+   src/test/resources/http/http-client.env.json > temp.json && mv temp.json src/test/resources/http/http-client.env.json
+
+echo "Tokens updated successfully!"
